0a6130a4fdbf39b4ab075360cdca05c6d39b8d7b
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-06-04 00:00:18 +0300

    initial commit

---
+++ GeneratingXMLRecords/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace GeneratingXMLRecords
+{
+    public class Startup
+    {
+
+        public static void Main()
+        {
+            var xmlGenerator = new XMLSupplier("channelRanklist");
+
+            XElement nova = xmlGenerator.CreateChannel("Nova", 1, "Nova Inc.", "Bulgaria", "Hippoland",
+                "Igrachki za vas.", "Didie", "Shosel", 12000000);
+
+            XElement btv = xmlGenerator.CreateChannel("BTV", 2, "BTV Media Group", "Bulgaria", "Zara",
+                "Style is key.", "Pavel", "Stanchev", 15050000);
+
+            XElement hbo = xmlGenerator.CreateChannel("HBO", 6, "HBO Group", "USA", "FOX",
+                "Just FOX description", "Richard", "Plepler", 32000000);
+
+            xmlGenerator.AddChannelToCatalog(btv);
+
+            xmlGenerator.SaveChanges();
+        }
+
+        
+    }
+}
---
+++ GeneratingXMLRecords/XMLSupplier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace GeneratingXMLRecords
+{
+    public class XMLSupplier
+    {
+        public static readonly string SavePath = "../../generated-channels.xml";
+
+        public XMLSupplier(string rootElementName)
+        {
+            this.RootElement = CreateCatalog(rootElementName);
+        }
+
+        private XElement RootElement { get; set; }
+
+        public XElement CreateChannel(string channelName, int rankPlace, string corporationName, string countryName,
+            string sponsorName, string sponsorDescription, string ownerFirstName, string ownerLastName,
+            int ownerNetWorth)
+        {
+            XElement channelXml = new XElement("channel",
+                new XElement("countryRanking", rankPlace),
+                new XElement("name", channelName),
+                new XElement("corporationName", corporationName),
+                new XElement("countryName", countryName),
+                new XElement("sponsor",
+                    new XElement("sponsorName", sponsorName),
+                    new XElement("sponsorDescription", sponsorDescription)
+                    ),
+                new XElement("owner",
+                    new XElement("firstName", ownerFirstName),
+                    new XElement("lastName", ownerLastName),
+                    new XElement("netWorth", ownerNetWorth))
+                    );
+
+            return channelXml;
+        }
+
+        private XElement CreateCatalog(string catalogName)
+        {
+            XElement catalog = new XElement(catalogName);
+            return catalog;
+        }
+
+        public void AddChannelToCatalog(XElement channel)
+        {
+            this.RootElement.Add(channel);
+        }
+
+        public void SaveChanges()
+        {
+            this.RootElement.Save(SavePath);
+        }
+    }
+}
